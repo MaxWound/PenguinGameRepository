964ea59c20d31035d5c1889167b7585753cb129c
MaxWound <max.wound1@gmail.com>
2022-04-14 22:38:51 +0300

    Сделал тестовый вариант спавна препятствий

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField]
+    GameObject testPrefab;
+    [SerializeField]
     GameObject floorPrefab;
     [SerializeField]
     Transform spawnTransform;
+    Tier[] Tiers;
+    private void Start()
+    {
+        Tiers = GetComponentsInChildren<Tier>();
+        for(int i = 0; i < Tiers.Length; i++)
+        {
+            Instantiate(testPrefab, Tiers[i].transform.position, Quaternion.identity);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         
